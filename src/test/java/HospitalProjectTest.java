5735f5f6a1bf40825208173cac867268fbb911b3
Sbsmith1101 <sbsmith1101@gmail.com>
2019-05-22 09:29:15 -0400

    added @before

--- src/test/java/HospitalProjectTest.java
+++ src/test/java/HospitalProjectTest.java
@@ -1,32 +1,40 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class HospitalProjectTest {
 
-	@Test
-	public void patientClassShouldExsist () {
+	Patient underTest = new Patient("");
+
+	@Before
+	public void setup() {
 		Patient underTest = new Patient("");
+	}
+
+	@Test
+	public void patientClassShouldExsist() {
 		String checkPatient = underTest.getPaitent();
 		assertEquals(null, checkPatient);
 	}
+
 	@Test
-	public void patientsShouldHaveAFirstName () {
+	public void patientsShouldHaveAFirstName() {
 		Patient underTest = new Patient("John");
 		String checkFisrtName = underTest.getFirstName();
 		assertEquals("John", checkFisrtName);
 	}
+
 	@Test
-	public void paitentShouldHaveABloodLevelDefault20 () {
-		Patient underTest = new Patient("");
+	public void paitentShouldHaveABloodLevelDefault20() {
 		int checkBloodLevel = underTest.getBloodLevel();
 		assertEquals(20, checkBloodLevel);
 	}
+
 	@Test
-	public void paitentShouldHaveAHealthLevelDefault10 () {
-		Patient underTest = new Patient("");
+	public void paitentShouldHaveAHealthLevelDefault10() {
 		int checkHealthLevel = underTest.getHealthLevel();
 		assertEquals(10, checkHealthLevel);
 	}
-	
+
 }
